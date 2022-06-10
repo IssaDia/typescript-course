30bc26bc57c6c51969243e1763eaf7650c4d9264
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-06-10 20:12:15 +0800

    end of Generics part

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,22 @@
+"use strict";
+const a = "Hello world";
+const b = 3;
+const c = true;
+const d = null;
+const arr = ["a", "b", "c"];
+const arr2 = [1, true, "hello"];
+const z = "Hello";
+const user = { firstname: "Leo", lastname: "Dubois" };
+function identity(arg) {
+    return arg;
+}
+const id = identity(3);
 const counter = document.querySelector("#counter");
 let i = 0;
 function increment() {
     i++;
-    counter.querySelector("span").innerText = i.toString();
+    const span = counter.querySelector("span");
+    if (span)
+        span.innerText = i.toString();
 }
-counter.addEventListener("click", increment);
+counter === null || counter === void 0 ? void 0 : counter.addEventListener("click", increment);
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,33 @@
-const counter = document.querySelector("#counter");
+const a: string = "Hello world";
+const b: number = 3;
+const c: boolean = true;
+const d: null = null;
+const arr: string[] = ["a", "b", "c"];
+const arr2:any[] = [1, true, "hello"];
+
+const z = "Hello";
+
+type User = {
+    firstname : string,
+    lastname : string
+}
+
+const user : User = {firstname : "Leo", lastname : "Dubois"};
+
+
+function identity<Argtype>(arg:Argtype) : Argtype {
+    return arg;
+}
+
+const id = identity<number>(3)
+
+const counter = document.querySelector("#counter") as HTMLButtonElement;
 let i = 0;
 function increment() {
   i++;
-  counter.querySelector("span").innerText = i.toString();
+  const span = counter.querySelector("span");
+
+  if(span) span.innerText = i.toString();
+
 }
-counter.addEventListener("click", increment);
+counter?.addEventListener("click", increment);
